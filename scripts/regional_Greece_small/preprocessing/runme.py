380ce207e417f3d6df2c33257e7535d84705728c
vivek-bala <b.vivek91@gmail.com>
2017-04-25 09:25:32 -0400

    checkpoint 6/8 substages done with kernel_fix branch of entk

--- scripts/regional_Greece_small/preprocessing/runme.py
+++ scripts/regional_Greece_small/preprocessing/runme.py
@@ -4,7 +4,7 @@ __license__   = "MIT"
 
 from radical.entk import EoP, AppManager, Kernel, ResourceHandle, PoE
 import argparse
-from pypaw_process_asdf import pypaw_process_asdf_kernel
+#from pypaw_process_asdf import pypaw_process_asdf_kernel
 #from pypaw_process import pypaw_process_kernel
 #from pypaw_window_selection import pypaw_window_selection_kernel
 #from pypaw_measure_adjoint import pypaw_measure_adjoint_kernel
@@ -13,10 +13,12 @@ from pypaw_process_asdf import pypaw_process_asdf_kernel
 #from pypaw_adjoint_asdf import pypaw_adjoint_asdf_kernel
 #from pypaw_sum_adjoint_asdf import pypaw_sum_adjoint_asdf_kernel
 
+pypaw_env = ['export PATH=/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin:$PATH',
+             'source activate pypaw_env']
 
 ENSEMBLE_SIZE=2
 
-class Seisflow(EoP):
+class Seisflow(PoE):
 
     def __init__(self, ensemble_size, pipeline_size):
         super(Seisflow,self).__init__(ensemble_size, pipeline_size)
@@ -32,20 +34,26 @@ class Seisflow(EoP):
             print '1:',instance
 
             k1 = Kernel(name="pypaw_process_asdf")
-            k1.arguments = ["--path=/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/ProcessObserved/C201002060444A.proc_obsd_17_40.path.json","--param=/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/ProcessObserved/proc_obsd.17_40.param.yml"]
+            k1.pre_exec = pypaw_env
+            k1.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-process_asdf"
+            k1.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/ProcessObserved/C201002060444A.proc_obsd_17_40.path.json',
+                            '-p', '/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/ProcessObserved/proc_obsd.17_40.param.yml']
             k1.cores = 16
             k1.mpi = True
 
             return k1
 
-        else:
+        elif instance==2:
 
             # Substage 2: Process raw synthetic data
 
             print instance
 
             k1 = Kernel(name="pypaw_process_asdf")
-            k1.arguments = ["--path=/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/ProcessSynthetic/C201002060444A.proc_synt_17_40.path.json","--param=/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/ProcessSynthetic/proc_synt.17_40.param.yml"]
+            k1.pre_exec = pypaw_env
+            k1.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-process_asdf"
+            k1.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/ProcessSynthetic/C201002060444A.proc_synt_17_40.path.json',
+                            '-p','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/ProcessSynthetic/proc_synt.17_40.param.yml']
             k1.cores = 16
             k1.mpi = True
 
@@ -57,9 +65,10 @@ class Seisflow(EoP):
         # Substage 3: Select time windows
 
         k2 = Kernel(name="pypaw_window_selection")
-        k2.arguments = ["--observed=","--synthetic=","--path=","--param="]
-        k2.link_input_data = ['$STAGE_1_TASK_1/observed.asdf','$STAGE_1_TASK_2/synthetic.asdf',
-                            'path.json','param.yml']
+        k2.pre_exec = pypaw_env
+        k2.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-window_selection_asdf"
+        k2.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/CreateWindows/C201002060444A.window.17_40.path.json',
+                        '-p', '/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/CreateWindows/window.17_40.param.yml']
         k2.cores = 16
         k2.mpi = True
 
@@ -71,9 +80,10 @@ class Seisflow(EoP):
         # Substage 4: Make measurements
 
         k3 = Kernel(name="pypaw_measure_adjoint")
-        k3.arguments = ["--observed=","--synthetic=","--window=","--path=","--param="]
-        k3.link_input_data = ['$STAGE_1_TASK_1/observed.asdf','$STAGE_1_TASK_2/synthetic.asdf',
-                            '$STAGE_2_TASK_1/window.json','path.json','param.yml']
+        k3.pre_exec = pypaw_env
+        k3.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-measure_adjoint_asdf"
+        k3.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/MeasureAdjoint/C201002060444A.measure_adj.17_40.path.json',
+                        '-p','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/MeasureAdjoint/adjoint.17_40.param.yml']
         k3.cores = 16
         k3.mpi = True
 
@@ -85,9 +95,10 @@ class Seisflow(EoP):
         # Substage 5: Filtering windows based on measurements
 
         k4 = Kernel(name="pypaw_filter_windows")
-        k4.arguments = ["--window=",'--measurements=','--stations=']
-        k4.link_input_data = ['$STAGE_2_TASK_1/window.json','$STAGE_3_TASK_1/measurements.json',
-                            'path.json','param.yml','stations.json']
+        k4.pre_exec = pypaw_env
+        k4.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-filter_windows"
+        k4.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/FilterWindows/C201002060444A.17_40.sensors.path.json',
+                        '-p','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/FilterWindows/filter_window.17_40.param.yml']
         k4.cores = 1
         k4.mpi = True
 
@@ -96,34 +107,33 @@ class Seisflow(EoP):
 
     def stage_5(self, instance):
 
-        if instance==1:
+        # Substage 6: Calculate weights for each measurements (window?)
 
-            # Substage 6: Calculate weights for each measurements (window?)
+        k5 = Kernel(name="pypaw_window_weights")
+        k5.pre_exec = pypaw_env
+        k5.executable = "/work/02734/vivek91/modules/miniconda2/envs/pypaw_env/bin/pypaw-window_weights"
+        k5.arguments = ['-f','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/paths/CreateWeights/C201002060444A.path.json',
+                        '-p','/work/02734/vivek91/modules/simpy/examples/titan_global_inv/params/CreateWeights/window_weights.param.yml']
+        k5.cores = 1
+        k5.mpi = True
 
-            k5 = Kernel(name="pypaw_window_weights")
-            k5.arguments = ["--filtered_window=","--stations=",'path.json','param.yml']
-            k5.link_input_data = ['$STAGE_4_TASK_1/filtered_window.json','path.json',
-                            'param.yml','stations.json']
-            k5.cores = 1
-            k5.mpi = True
-
-            return k5
-
-        else:
-
-            # Substage 7: Calculate adjoint sources
-
-            k5 = Kernel(name="pypaw_adjoint_asdf")
-            k5.arguments = ["--observed=","--synthetic=","--filtered_window=","--path=","--param="]
-            k5.link_input_data = ['$STAGE_1_TASK_1/observed.asdf','$STAGE_1_TASK_2/synthetic.asdf',
-                            '$STAGE_4_TASK_1/filtered_window.json','path.json','param.yml']
-            k5.cores = 16
-            k5.mpi = True
-
-            return k5
+        return k5
 
 
     def stage_6(self, instance):
+
+        # Substage 7: Calculate adjoint sources
+
+        k5 = Kernel(name="pypaw_adjoint_asdf")
+        k5.arguments = ["--observed=","--synthetic=","--filtered_window=","--path=","--param="]
+        
+        k5.cores = 16
+        k5.mpi = True
+
+        return k5
+
+
+    def stage_7(self, instance):
 
         # Substage 8: Combine weights and adjoint sources
 
@@ -143,9 +153,6 @@ if __name__ == '__main__':
 
     # Create an application manager
     app = AppManager(name='preprocessing')
-
-    # Register kernels to be used
-    app.register_kernels(pypaw_process_asdf_kernel)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--resource', help='target resource label')
@@ -186,7 +193,7 @@ if __name__ == '__main__':
         res.allocate(wait=True)
 
         # Create pattern object with desired ensemble size, pipeline size
-        pipe = Seisflow(ensemble_size=2, pipeline_size=1)
+        pipe = Seisflow(ensemble_size=[2,1,1,1,1], pipeline_size=5)
 
         # Add workload to the application manager
         app.add_workload(pipe)
