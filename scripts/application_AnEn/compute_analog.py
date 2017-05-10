99bc41feadfb6855609db7f85184a763db1a0a6c
vivek-bala <b.vivek91@gmail.com>
2017-05-10 16:17:36 -0400

    fixing arg pars

--- scripts/application_AnEn/compute_analog.py
+++ scripts/application_AnEn/compute_analog.py
@@ -3,14 +3,30 @@ from radical.ensemblemd.kernel_plugins.kernel_base import KernelBase
 _KERNEL_INFO = {
     "name":         "compute_analog_for_one_station",                  # Mandatory
     "description":  "sleeping kernel",        # Optional
-    "arguments":   {},
+    "arguments":   {
+                '--data=': {
+                                "mandatory": True,                # Mandatory argument? True or False
+                                "description": "Location of the data"
+    	                    },
+                '--output=': {
+                                "mandatory": True,                # Mandatory argument? True or False
+                                "description": "Location of output"
+    	                    },
+
+                '--stations=': {
+                                "mandatory": True,                # Mandatory argument? True or False
+                                "description": "Number of stations"
+    	                    },
+
+
+    },
     "machine_configs":                        # Use a dictionary with keys as
         {                                     # resource names and values specific
             "local.localhost":                # to the resource
             {
                 "environment" : None,         # dict or None, can be used to set env variables
-                "pre_exec"    : None,         # list or None, can be used to load modules
-                "executable"  : ["/bin/sleep"],        # specify the executable to be used
+                "pre_exec"    : ['export PATH=$PATH:/Users/wuh20/Desktop'], # list or None, can be used to load modules
+                "executable"  : 'canalogsmp',        # specify the executable to be used
                 "uses_mpi"    : False         # mpi-enabled? True or False
             },
         }
@@ -24,22 +40,29 @@ class compute_analog_kernel(KernelBase):
 
         super(compute_analog_kernel, self).__init__(_KERNEL_INFO)
      	"""Le constructor."""
-        		
+
     # --------------------------------------------------------------------------
     #
     @staticmethod
     def get_name():
         return _KERNEL_INFO["name"]
-        
+
 
     def _bind_to_resource(self, resource_key):
         """This function binds the Kernel to a specific resource defined in
         "resource_key".
-        """        
-        arguments  = ["-L -l -d /Users/wuh20/data/data_windLuca -o results_for_station_1.txt --stations 1"]
+        """
 
-        self._executable  = '/Users/wuh20/Desktop/canalogsmp'
+        cfg = _KERNEL_INFO["machine_configs"][resource_key]
+
+        arguments  = ['-L','-l',
+                      '-d', self.get_arg('--data='),
+                      '-o', self.get_arg('--output='),
+                      '--stations',self.get_arg('--stations=')
+                    ]
+
+        self._executable  = cfg['executable']
         self._arguments   = arguments
-        self._environment = None 
+        self._environment = None
         self._uses_mpi    = None
-        self._pre_exec    = None
+        self._pre_exec    = cfg['pre_exec']
