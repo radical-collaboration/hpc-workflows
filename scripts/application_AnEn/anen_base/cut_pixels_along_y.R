b1ce6b71f8677180fab9888381e20dca661ac3de
Weiming <cosmos.weiming@outlook.com>
2017-09-26 10:42:24 -0400

    change variable types

--- scripts/application_AnEn/anen_base/cut_pixels_along_y.R
+++ scripts/application_AnEn/anen_base/cut_pixels_along_y.R
@@ -7,6 +7,15 @@ cut_pixels_along_y <- function(
    ygrids.total, start = 0) {
     require(RAnEnExtra)
 
+    # convert variable types
+    #pixels.compute <- as.numeric(pixels.compute)
+    #ycuts <- as.numeric(ycuts)
+
+    xgrids.total <- as.numeric(xgrids.total)
+    ygrids.total <- as.numeric(ygrids.total)
+    start <- as.numeric(start)
+
+
     pixels.cut <- cut.pixels.along.y(pixels.compute, ycuts,
                                      xgrids.total, ygrids.total, 0)
     return(pixels.cut)
