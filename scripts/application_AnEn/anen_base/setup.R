5602edabd9b01b09cad0d4fd98fe50484eb822fb
vivek-bala <b.vivek91@gmail.com>
2017-09-25 20:42:10 -0400

    more reorg

--- scripts/application_AnEn/anen_base/setup.R
+++ scripts/application_AnEn/anen_base/setup.R
@@ -1,75 +1,62 @@
-# set up file for ENTK and AnEN
-#
-# this file configures the initial arguments for the AnEn executable
-# the function will be called from the master.py script
-#
-initial_config <- function (verbose = F) {
-    current_stage <- 1
-    init.num.pixels <- 100
-    nrows <- 100
-    ncols <- 100
+##########
+# set up #
+##########
+# set up basic parameters that would be shared by all processes
+command.exe <- '~/github/CAnalogsV2/install/bin/canalogs'
+command.verbose <- '--verbose 0'
+file.forecasts <- "~/geolab_storage_V2/data/NAM12KM/chunk_NAM/Forecasts_NAM_sliced.nc"
+file.observations <- "~/geolab_storage_V2/data/NAM12KM/chunk_NAM/analysis_NAM.nc"
+folder.prefix <- '~/geolab_storage_V2/data/NAM12KM/experiments_smart/'
+folder.accumulate <- paste(folder.prefix, 'anen_accumulate/', sep = '')
+folder.output <- paste(folder.prefix, 'anen_output/', sep = '')
+folder.raster.anen <- paste(folder.prefix, 'anen_raster/', sep = '')
+folder.raster.obs <- paste(folder.prefix, 'obs_raster/', sep = '')
+folder.tmp <- paste(folder.prefix, 'tmp/', sep = '')
 
-    file.forecast <- '/home/whu/data/Temperature_NAM/Forecasts_NAM_C.nc'
-    file.observation <- '/home/whu/data/Temperature_NAM/Analysis_NAM_R.nc'
-
-    num.flts <- 4
-    num.times <- 822
-    num.parameters <- 13
-    num.stations.per.chunk <- 10000
-    num.stations <- 262790
-
-    test.ID.start <- 700
-    test.ID.end <- 701
-    train.ID.start <- 0
-    train.ID.end <- 699
-    members.size <- 20
-    rolling <- -2
-    cores <- 16
-
-    output.prefix <- paste('/work/whu/', current_stage, '_', sep = '')
-    output.AnEn <- paste(output.prefix, 'anen.nc', sep = '')
-    #output.computed.pixels <- paste(output.prefix, 'computed_pixels.rdata', sep = '')
-    #stations.ID <- sample.int(nrows * ncols, init.num.pixels)
-
-    # make sure that the points at the four corners are included
-    #if (!(0 %in% stations.ID)) {
-    #    stations.ID[1] <- 0
-    #}
-    #if (!(99 %in% stations.ID)) {
-    #    stations.ID[2] <- 99
-    #}
-    #if (!(9900 %in% stations.ID)) {
-    #    stations.ID[3] <- 9900
-    #}
-    #if (!(9999 %in% stations.ID)) {
-    #    stations.ID[4] <- 9999
-    #}
-
-    # save the index numbers of computed pixels
-    #save(stations.ID, file = output.computed.pixels)
-
-    list.init.config <- list(file.forecast = file.forecast,                             
-                             file.observation = file.observation,
-                             output.AnEn = output.AnEn,
-                             #stations.ID = stations.ID,
-                             test.ID.start = test.ID.start,
-                             test.ID.end = test.ID.end,
-                             train.ID.start = train.ID.start,
-                             train.ID.end = train.ID.end,
-                             rolling = rolling,
-                             members.size = members.size,
-                             cores = cores,
-                             num.flts = num.flts,
-                             num.times = num.times,
-                             num.parameters = num.parameters,
-                             num.stations.per.chunk = num.stations.per.chunk,
-                             num.stations = num.stations,
-                             nrows = nrows,
-                             ncols = ncols)
-
-    if (verbose) {
-        print(list.init.config)
-    }
-  
-    return(list.init.config)
+file.pixels.computed <- paste(folder.prefix, 'pixels_computed_list.rdata', sep = '')
+if(!file.exists(file.pixels.computed)) {
+  pixels.computed.list <- list()
+  save(pixels.computed.list, file = file.pixels.computed)
 }
+
+num.flts <- 4
+num.times <- 822
+num.times.to.compute <- 2
+num.parameters <- 13
+ygrids.total <- 428
+xgrids.total <- 614
+grids.total <- xgrids.total*ygrids.total
+init.num.pixels.compute <- 100
+yinterval <- 15
+ycuts <- seq(from = 1, to = ygrids.total, by = yinterval)
+
+quick <- F
+cores <- 8
+rolling <- 0
+observation.ID <- 0
+train.ID.start <- 0
+train.ID.end <- 699
+test.ID.start <- 700
+test.ID.end <- test.ID.start + num.times.to.compute - 1
+weights <- rep(1, num.parameters)
+members.size <- 20
+
+num.neighbors <- 2
+iteration <- '0001'
+threshold.triangle <- 2
+num.pixels.increase <- 10
+
+debug <- FALSE
+
+rast.base <- raster(nrows = ygrids.total, ncols = xgrids.total,
+                    xmn = 0.5, xmx = xgrids.total+.5,
+                    ymn = 0.5, ymx = ygrids.total+.5)
+
+for(folder in c(folder.accumulate, folder.output,
+                folder.raster.anen, folder.raster.obs)) {
+  if (!dir.exists(folder)) {
+    dir.create(folder, recursive = T)
+  }
+}
+
+
