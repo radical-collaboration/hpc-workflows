4d9076056846e6b5c0fc012cef1e44e89f1a2382
Weiming <huweiming950714@gmail.com>
2018-09-25 12:04:47 -0600

    modify test

--- scripts/application_AnEn/year_2/test_1.py
+++ scripts/application_AnEn/year_2/test_1.py
@@ -1,6 +1,6 @@
 # This is the integration test for Analog Ensemble and Ensemble Toolkit on Cheyenne supercomputer.
 
-from radical.entk import Pipeline, Stage, Task, AppManager, ResourceManager
+from radical.entk import Pipeline, Stage, Task, AppManager 
 
 if __name__ == "__main__":
 	print "Hello world!"
