76c69bad8aa1a3b3eb02548e4fb5ccc8129e8755
Weiming-Hu <cosmos.weiming@outlook.com>
2019-01-21 17:30:20 -0500

    work on multinode py code

--- scripts/application_AnEn/year_2/multi-node/runme.py
+++ scripts/application_AnEn/year_2/multi-node/runme.py
@@ -1,143 +1,173 @@
 import os
 import sys
 import yaml
+import datetime
 import argparse
+from pprint import pprint
 from radical.entk import Pipeline, Stage, Task, AppManager
 
 
+def get_months_between(start, end, format="%Y%m", reverse=False):
+    start = datetime.datetime.strptime(start, format)
+    end = datetime.datetime.strptime(end, format)
 
-def create_sd_calc_task(i, task_desc):
+    # Add one day to the end to make the range inclusive
+    dates = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days + 1)]
+    dates_str = [i.strftime(format) for i in dates]
 
+    # Only keep the unique strings
+    months = list(set(dates_str))
+    months.sort(reverse=reverse)
+
+    return months
+
+
+def task_sd_calc(i, stage_cfg, global_cfg):
     t = Task()
-    t.name = 'task-%s'%i
-    t.pre_exec = task_desc['pre_exec']
-    t.executable = task_desc['executable']
+    t.name = 'task-sd-calc-{:05d}'.format(i)
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
     t.cpu_threads = {
-        'processes': task_desc['cpu']['processes'],
-        'process_type': task_desc['cpu']['process_type'],
-        'threads_per_process': task_desc['cpu']['threads_per_process'],
-        'thread_type': task_desc['cpu']['thread_type'],
+        'processes': stage_cfg['cpu']['processes'],
+        'process-type': stage_cfg['cpu']['process-type'],
+        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
+        'thread-type': stage_cfg['cpu']['thread-type'],
     }
-    t.arguments = [
-        # The following arguments solely depend on the configuration file
-        '--in', task_desc['args']['in'],
-        '--out', task_desc['args']['out'],
-        '--verbose', task_desc['args']['verbose'],
 
-        # The following arguments depend on the configuration file and the task number i
-        '--start', task_desc['args']['start'],
-        '--count', task_desc['args']['count'],
+    # Select input files based on the search month range
+    months = get_months_between(global_cfg['search-month-start'], global_cfg['search-month-end'])
+
+    # Append the data folder prefix and the file format suffix
+    in_files = ['{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc') for month in months]
+    out_file = '{}{}{}'.format(global_cfg['sds-folder'], t.name, '.nc')
+
+    t.arguments = [
+        '--in', in_files,
+        '--out', out_file,
+        '--verbose', stage_cfg['args']['verbose'],
+        # '--start', stage_cfg['args']['start'],
+        # '--count', stage_cfg['args']['count'],
     ]
 
     t.link_input_data = []
     t.copy_output_data = []
 
+    pprint(t.to_dict())
+
     return t
 
 
-
-def create_sim_calc_task(i, task_desc):
-
+def task_sim_calc(i, month, stage_cfg, global_cfg):
     t = Task()
-    t.name = 'task-%s'%i
-    t.pre_exec = task_desc['pre_exec']
-    t.executable = task_desc['executable']
+    t.name = 'task-sims-calc-{:05d}'.format(i)
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
     t.cpu_threads = {
-                        'processes': task_desc['cpu']['processes'],
-                        'process_type': task_desc['cpu']['process_type'],
-                        'threads_per_process': task_desc['cpu']['threads_per_process'],
-                        'thread_type': task_desc['cpu']['thread_type'],
-                    }
+        'processes': stage_cfg['cpu']['processes'],
+        'process-type': stage_cfg['cpu']['process-type'],
+        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
+        'thread-type': stage_cfg['cpu']['thread-type'],
+    }
     t.arguments = [
-        # The following arguments solely depend on the configuration file
-                    '--test-forecast-nc', task_desc['args']['test-forecast-nc'],
-                    '--search-forecast-nc', task_desc['args']['search-forecast-nc'],
-                    '--observation-nc', task_desc['args']['observation-nc'],
-                    '--similarity-nc', task_desc['args']['similarity-nc'],
-                    '--verbose', task_desc['args']['verbose'],
-                    '--time-match-mode', task_desc['args']['time-match-mode'],
-                    '--observation-id', task_desc['args']['observation-id'],
-                    '--sds-nc', task_desc['args']['sds-nc'],
+        '--test-forecast-nc', stage_cfg['args']['test-forecast-nc'],
+        '--search-forecast-nc', '{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc'),
+        '--observation-nc', '{}{}{}'.format(global_cfg['observations-folder'], month, '.nc'),
+        '--similarity-nc', '{}{}-{:05d}{}'.format(global_cfg['sims-folder'], month, i, '.nc'),
+        '--verbose', stage_cfg['args']['verbose'],
+        '--observation-id', global_cfg['observation-id'],
+        '--sds-nc', '{}task-sd-calc-{:05d}{}'.format(global_cfg['sds-folder'], i, '.nc')
 
-        # The following arguments depend on the configuration file and the task number i
-                    '--test-start', task_desc['args']['test-start'],
-                    '--test-count', task_desc['args']['test-count'],
-                    # '--search-start', task_desc['args']['search-start'],
-                    # '--search-count', task_desc['args']['search-count'],
-                    # '--obs-start', task_desc['args']['obs-start'],
-                    # '--obs-count', task_desc['args']['obs-count'],
-                    # '--sds-start', task_desc['args']['sds-start'],
-                    # '--sds-count', task_desc['args']['sds-count']
-                ]
+        #'--test-start', stage_cfg['args']['test-start'],
+        #'--test-count', stage_cfg['args']['test-count'],
+        # '--search-start', stage_cfg['args']['search-start'],
+        # '--search-count', stage_cfg['args']['search-count'],
+        # '--obs-start', stage_cfg['args']['obs-start'],
+        # '--obs-count', stage_cfg['args']['obs-count'],
+        # '--sds-start', stage_cfg['args']['sds-start'],
+        # '--sds-count', stage_cfg['args']['sds-count']
+    ]
 
     t.link_input_data = []
     t.copy_output_data = []
 
+    pprint(t.to_dict())
     return t
 
 
-
-def create_analog_select_task(i, task_desc):
+def create_analog_select_task(i, month, stage_cfg, global_cfg):
     t = Task()
-    t.name = 'task-%s'%i
-    t.pre_exec = task_desc['pre_exec']
-    t.executable = task_desc['executable']
+    t.name = 'task-analog-select-{:05d}'.format(i)
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
     t.cpu_threads = {
-                        'processes': task_desc['cpu']['processes'],
-                        'process_type': task_desc['cpu']['process_type'],
-                        'threads_per_process': task_desc['cpu']['threads_per_process'],
-                        'thread_type': task_desc['cpu']['thread_type'],
-                    }
+        'processes': stage_cfg['cpu']['processes'],
+        'process-type': stage_cfg['cpu']['process-type'],
+        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
+        'thread-type': stage_cfg['cpu']['thread-type'],
+    }
     t.arguments = [
-                    '--config', task_desc['args']['config'],
-                    '--similarity-nc', task_desc['args']['similarity-nc'],
-                    '--observation-nc', task_desc['args']['observation-nc'],
-                    '--mapping-txt', task_desc['args']['mapping-txt'],
-                    '--analog-nc', task_desc['args']['analog-nc'],
-                    '--members', task_desc['args']['members'],
-                    '--verbose', task_desc['args']['verbose'],
-                    '--observation-id', task_desc['args']['observation-id'],
-                    '--obs-start', task_desc['args']['obs-start'],
-                    '--obs-count', task_desc['args']['obs-count'],
-                    '--quick', task_desc['args']['quick'],
-                    '--extend-obs', task_desc['args']['extend-obs'],
-                    '--real-time', task_desc['args']['real-time'],
-                ]
+        '--quick', stage_cfg['args']['quick'],
+        '--members', stage_cfg['args']['members'],
+        '--verbose', stage_cfg['args']['verbose'],
+        '--observation-id', global_cfg['observation-id'],
+
+        '--similarity-nc', '{}{}-{:05d}{}'.format(global_cfg['sims-folder'], month, i, '.nc'),
+        '--observation-nc', '{}{}{}'.format(global_cfg['observations-folder'], month, '.nc'),
+        '--analog-nc', '{}{}-{:05d}{}'.format(global_cfg['analogs-folder'], month, i, '.nc'),
+
+        #'--obs-start', stage_cfg['args']['obs-start'],
+        #'--obs-count', stage_cfg['args']['obs-count'],
+    ]
 
     t.link_input_data = []
     t.copy_output_data = []
+    pprint(t.to_dict())
 
     return t
-
 
 
 def create_pipelines(wcfg):
-    p = Pipeline
+    p = Pipeline()
 
-    # Create first stage for similarity calculator tasks
-    s1 = Stage()
-    s1.name = 'stage-1'
-    stage_cfg = wcfg['stage_sim_cal']
+    # Create the stage for standard deviation calculator tasks
+    s = Stage()
+    s.name = 'stage-sd-calc'
+    stage_cfg = wcfg[s.name]
 
-    for i in range(stage_cfg['task_count']):
-        t = create_sim_calc_task(i, stage_cfg['task_desc'])
-        s1.add_tasks(t)
+    for i in range(wcfg['global']['task-count']):
+        t = task_sd_calc(i, stage_cfg, wcfg['global'])
+        s.add_tasks(t)
 
-    p.add_stages(s1)
+    p.add_stages(s)
 
-    # Create second stage for analog selector tasks
-    s2 = Stage()
-    s2.name = 'stage-2'
-    stage_cfg = wcfg['stage_analog_select']
+    # Create the stage for similarity calculator tasks
+    s = Stage()
+    s.name = 'stage-sim-calc'
+    stage_cfg = wcfg[s.name]
 
-    for i in range(stage_cfg['task_count']):
-        t = create_analog_select_task(i, stage_cfg['task_desc'])
-        s2.add_tasks(t)
+    # Get the number of forecast files to be used. This is the number of months specified.
+    months = get_months_between(wcfg['global']['search-month-start'], wcfg['global']['search-month-end'])
 
-    p.add_stages(s2)
+    for i in range(wcfg['global']['task-count']):
+        for j in range(len(months)):
+            t = task_sim_calc(i, months[j], stage_cfg, wcfg['global'])
+            s.add_tasks(t)
+
+    p.add_stages(s)
+
+    # Create the stage for analog selector tasks
+    s = Stage()
+    s.name = 'stage-analog-select'
+    stage_cfg = wcfg[s.name]
+
+    for i in range(wcfg['global']['task-count']):
+        for j in range(len(months)):
+            t = create_analog_select_task(i, months[j], stage_cfg, wcfg['global'])
+            s.add_tasks(t)
+
+    p.add_stages(s)
 
     return p
-
 
 
 if __name__ == '__main__':
@@ -148,30 +178,36 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     if not os.path.isfile(args.wcfg):
-        print '%s does not exist'%args.wcfg
+        print '%s does not exist' % args.wcfg
         sys.exit(1)
 
     if not os.path.isfile(args.rcfg):
-        print '%s does not exist'%args.rcfg
+        print '%s does not exist' % args.rcfg
         sys.exit(1)
 
-    with open(args.rcfg,'r') as fp:
+    with open(args.rcfg, 'r') as fp:
         rcfg = yaml.load(fp)
 
-    with open(args.wcfg,'r') as fp:
+    with open(args.wcfg, 'r') as fp:
         wcfg = yaml.load(fp)
 
-    amgr = AppManager(  hostname=rcfg['rabbitmq']['hostname'], 
-                        port=rcfg['rabbitmq']['port'])
+    with open('./resource_cfg.yml', 'r') as fp:
+        rcfg = yaml.load(fp)
+
+    with open('./workflow_cfg.yml', 'r') as fp:
+        wcfg = yaml.load(fp)
+
+    amgr = AppManager(hostname=rcfg['rabbitmq']['hostname'],
+                      port=rcfg['rabbitmq']['port'])
 
     res_desc = {
-            'resource': rcfg['resource_desc']['name'],
-            'walltime': rcfg['resource_desc']['walltime'],
-            'cpus': rcfg['resource_desc']['cpus'],
-            'queue': rcfg['resource_desc']['queue'],
-            'project': rcfg['resource_desc']['project']
-        }
-    
+        'resource': rcfg['resource-desc']['name'],
+        'walltime': rcfg['resource-desc']['walltime'],
+        'cpus': rcfg['resource-desc']['cpus'],
+        'queue': rcfg['resource-desc']['queue'],
+        'project': rcfg['resource-desc']['project']
+    }
+
     amgr.resource_desc = res_desc
 
     pipelines = create_pipelines(wcfg)
@@ -180,4 +216,4 @@ if __name__ == '__main__':
 
     amgr.workflow = pipelines
 
-    # amgr.run()
+    #amgr.run()
