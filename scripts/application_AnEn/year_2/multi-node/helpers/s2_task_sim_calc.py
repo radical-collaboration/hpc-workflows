2eb30e95c93af6efee6d2e85944b657eedc4d693
Vivekanandan (Vivek) Balasubramanian <vivek.balasubramanian@rutgers.edu>
2019-02-11 10:44:57 -0500

    some restructuring

---
+++ scripts/application_AnEn/year_2/multi-node/helpers/s2_task_sim_calc.py
@@ -0,0 +1,67 @@
+from radical.entk import Task
+from utils import extract_month, get_indices
+from pprint import pprint
+
+def task_sim_calc(i, month, stage_cfg, global_cfg, files_dims):
+    """
+    This function creates a similarity calculation task for the specified task number.
+
+    :param i: The task number.
+    :param month: The string for year and month, for example, 201001.
+    :param stage_cfg: The configuration dictionary for this stage.
+    :param global_cfg: The global configuration dictionary.
+    :param files_dims: The dimension information generated from the function get_files_dims.
+    :return: A Task object.
+    """
+
+    t = Task()
+    t.name = 'task-sims-calc-{:05d}'.format(i)
+
+    if global_cfg['print-progress']:
+        print "Creating similarity task {}".format(t.name)
+
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
+    t.cpu_threads = {
+        'processes': stage_cfg['cpu']['processes'],
+        'process-type': stage_cfg['cpu']['process-type'],
+        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
+        'thread-type': stage_cfg['cpu']['thread-type'],
+    }
+
+    # Extract the test month from the test forecast file path
+    test_month = extract_month(global_cfg['test-forecast-nc'])
+
+    # Calculate the indices for starts and counts
+    [test_starts, test_counts] = get_indices('forecasts', test_month, i, files_dims, global_cfg)
+    [search_starts, search_counts] = get_indices('forecasts', month, i, files_dims, global_cfg)
+    [obs_starts, obs_counts] = get_indices('observations', month, i, files_dims, global_cfg)
+
+    t.arguments = [
+        '--test-forecast-nc', global_cfg['test-forecast-nc'],
+        '--search-forecast-nc', '{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc'),
+        '--observation-nc', '{}{}{}'.format(global_cfg['observations-folder'], month, '.nc'),
+        '--similarity-nc', '{}{}-{:05d}{}'.format(global_cfg['sims-folder'], month, i, '.nc'),
+        '--verbose', stage_cfg['args']['verbose'],
+        '--observation-id', global_cfg['observation-id'],
+        '--sds-nc', '{}task-sd-calc-{:05d}{}'.format(global_cfg['sds-folder'], i, '.nc'),
+    ]
+
+    # Add list arguments
+    t.arguments.append('--test-start'); t.arguments.extend(test_starts)
+    t.arguments.append('--test-count'); t.arguments.extend(test_counts)
+    t.arguments.append('--search-start'); t.arguments.extend(search_starts)
+    t.arguments.append('--search-count'); t.arguments.extend(search_counts)
+    t.arguments.append('--obs-start'); t.arguments.extend(obs_starts)
+    t.arguments.append('--obs-count'); t.arguments.extend(obs_counts)
+
+    if global_cfg['print-help']:
+        t.arguments.extend(['-h'])
+
+    t.link_input_data = []
+    t.copy_output_data = []
+
+    if global_cfg['print-tasks-only']:
+        pprint(t.to_dict())
+
+    return t
--- scripts/application_AnEn/year_2/multi-node/runme.py
+++ scripts/application_AnEn/year_2/multi-node/runme.py
@@ -1,397 +1,14 @@
-import re
 import os
 import sys
 import yaml
 import argparse
-import datetime
-from pprint import pprint
-from netCDF4 import Dataset
-from radical.entk import Pipeline, Stage, Task, AppManager
-
-def check_empty(global_cfg):
-    empty = True
-
-    if len(os.listdir(global_cfg['sds-folder'])) != 0:
-        print "Directory {} is not empty.".format(global_cfg['sds-folder'])
-        empty = False
-
-    if len(os.listdir(global_cfg['sims-folder'])) != 0:
-        print "Directory {} is not empty.".format(global_cfg['sims-folder'])
-        empty = False
-
-    if len(os.listdir(global_cfg['analogs-folder'])) != 0:
-        print "Directory {} is not empty.".format(global_cfg['analogs-folder'])
-        empty = False
-
-    return empty
-
-
-def extract_month(file_path, pattern = '.*?/(\d{6})\.nc$'):
-    """
-    This function extracts the month string from a file path using the specified pattern.
-
-    :param file_path The string of the file path.
-    :param pattern The pattern string to be used.
-    :return: A string of the year and month, for example, 201001.
-    """
-
-    month = re.search(pattern, file_path)
-
-    if month is None:
-        sys.exit('Cannot extract month from the file path {}'.format(file_path))
-
-    return month.group(1)
-
-def expand_tilde(cfg):
-    """
-    This function uses os.path.expanduser() to expand the tilde sign.
-    First, get the expanded path of tilde, and then replace tildes in all strings
-    contained in the input dict.
-
-    :param cfg: A dictionary.
-    :return: A dictionary.
-    """
-
-    # Get the expanded path of the tilde sign
-    path = os.path.expanduser('~')
-
-    # Match the tilde sign and change
-    def iterdict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                iterdict(v)
-            else:
-                if isinstance(v, str) and "~" in v:
-                    d[k] = v.replace('~',path)
-                elif isinstance(v, list):
-                    d[k] = [x if '~' not in x else x.replace('~',path) for x in v]
-
-    iterdict(cfg)
-    return cfg
-
-
-def get_months_between(start, end, format="%Y%m", reverse=False):
-    """
-    This functions generate a month sequence of strings between the specified dates
-    with the input format.
-
-    :param start: The string for the start of the month, for example, 201001.
-    :param end: The string for the end of the month, for example, 201010.
-    :param format: The format of the input and output month strings.
-    :param reverse: Whether to reverse order the sequence.
-    :return: A list of month strings.
-    """
-
-    start = datetime.datetime.strptime(start, format)
-    end = datetime.datetime.strptime(end, format)
-
-    # Add one day to the end to make the range inclusive
-    dates = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days + 1)]
-    dates_str = [i.strftime(format) for i in dates]
-
-    # Only keep the unique strings
-    months = list(set(dates_str))
-    months.sort(reverse=reverse)
-
-    return months
-
-
-def get_files_dims(global_cfg, check_dims=True):
-    """
-    This functions reads and records the dimension information of test and search files.
-    Observation and forecast search files are defined by global arguments.
-
-    :param global_cfg: A dictionary with global arguments.
-    :param check_dims: Whether to check dimensions.
-    :return: A dictionary of dimension information for each month of observations and forecasts.
-    """
-
-    # Get a list of search months
-    months = get_months_between(global_cfg['search-month-start'], global_cfg['search-month-end'])
-
-    # Define the dimensions for both forecast and observation files
-    files_dims = dict.fromkeys(["forecasts", "observations"])
-    files_dims['forecasts'] = dict.fromkeys(months)
-    files_dims['observations'] = dict.fromkeys(months)
-
-    # Read dimensions from forecast files
-    forecast_files = ['{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc') for month in months]
-
-    # Add the forecast file
-    month = extract_month(global_cfg['test-forecast-nc'])
-    months.append(month)
-    forecast_files.append('{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc'))
-
-    for i in range(len(forecast_files)):
-        if global_cfg['print-progress']:
-            sys.stdout.write('Processing {}'.format(forecast_files[i]))
-            sys.stdout.flush()
-
-        nc = Dataset(forecast_files[i])
-        dims = (len(nc.dimensions['num_parameters']), len(nc.dimensions['num_stations']),
-                len(nc.dimensions['num_times']), len(nc.dimensions['num_flts']))
-        nc.close()
-
-        if check_dims:
-            if (dims[0] != global_cfg['num-forecast-parameters']) or \
-                    (dims[1] != global_cfg['num-grids']) or \
-                    (dims[3] != global_cfg['num-flts']):
-                sys.exit("File ({}) does not meet the dimension requirement.".format(forecast_files[i]))
-
-        files_dims['forecasts'][months[i]] = dims
-
-        if global_cfg['print-progress']:
-            sys.stdout.write(' Done!\n')
-
-    # Read dimensions from observation files
-    observation_files = ['{}{}{}'.format(global_cfg['observations-folder'], month, '.nc') for month in months]
-
-    for i in range(len(observation_files)):
-        if global_cfg['print-progress']:
-            sys.stdout.write('Processing {}'.format(observation_files[i]))
-            sys.stdout.flush()
-
-        nc = Dataset(observation_files[i])
-        dims = (len(nc.dimensions['num_parameters']),
-                len(nc.dimensions['num_stations']),
-                len(nc.dimensions['num_times']))
-        nc.close()
-
-        if check_dims:
-            if (dims[0] != global_cfg['num-observation-parameters']) or (dims[1] != global_cfg['num-grids']):
-                sys.exit("File ({}) does not meet the dimension requirement.".format(forecast_files[i]))
-
-        files_dims['observations'][months[i]] = dims
-
-        if global_cfg['print-progress']:
-            sys.stdout.write(' Done!\n')
-
-    return files_dims
-
-
-def get_indices(type, month, task_i, files_dims, global_cfg):
-    """
-    This function generates the indices to be used by PEF based on the task number and the month.
-    Currently, the dimension of grids are distributed among tasks.
-
-    :param type: A string of either 'forecasts' or 'observations'.
-    :param month: A string of year and month, for example, 201001.
-    :param task_i: The task number.
-    :param files_dims: The dimension information generated from the function get_files_dims.
-    :param global_cfg: The global configuration dictionary
-    :return: A list of indices
-    """
-
-    # Add the index for parameters
-    starts = [0]
-    if type is "forecasts":
-        counts = [global_cfg['num-forecast-parameters']]
-    elif type is "observations":
-        counts = [global_cfg['num-observation-parameters']]
-    else:
-        sys.exit("Unrecognized type {}".format(type))
-
-    # Add the index for stations
-    starts.append(int(global_cfg['num-grids'] / global_cfg['task-count']) * task_i)
-    if task_i == global_cfg['task-count'] - 1:
-        counts.append(global_cfg['num-grids'] - int(global_cfg['num-grids'] / global_cfg['task-count']) * task_i)
-    else:
-        counts.append(int(global_cfg['num-grids'] / global_cfg['task-count']))
-
-    # Add the index for times
-    starts.append(0)
-    counts.append(files_dims[type][month][2])
-
-    # Add the index for FLTs
-    if type is 'forecasts':
-        starts.append(0)
-        counts.append(global_cfg['num-flts'])
-
-    return [starts, counts]
-
-
-def task_sd_calc(i, stage_cfg, global_cfg, files_dims):
-    """
-    This function creates a standard deviation task for the specified task number.
-
-    :param i: The task number.
-    :param stage_cfg: The configuration for this stage.
-    :param global_cfg: The global configuration.
-    :param files_dims: The dimension information generated from the function get_files_dims.
-    :return: A Task object.
-    """
-    t = Task()
-    t.name = 'task-sd-calc-{:05d}'.format(i)
-
-    if global_cfg['print-progress']:
-        print "Creating standard deviation task {}".format(t.name)
-
-    t.pre_exec = stage_cfg['pre-exec']
-    t.executable = stage_cfg['executable']
-    t.cpu_threads = {
-        'processes': stage_cfg['cpu']['processes'],
-        'process-type': stage_cfg['cpu']['process-type'],
-        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
-        'thread-type': stage_cfg['cpu']['thread-type'],
-    }
-
-    # Select input files based on the search month range
-    months = get_months_between(global_cfg['search-month-start'], global_cfg['search-month-end'])
-
-    # Append the data folder prefix and the file format suffix
-    in_files = ['{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc') for month in months]
-    out_file = '{}{}{}'.format(global_cfg['sds-folder'], t.name, '.nc')
-
-    # Calculate the indices for starts and counts
-    index_starts = []; index_counts = []
-
-    for month in months:
-        [starts, counts] = get_indices('forecasts', month, i, files_dims, global_cfg)
-        index_starts.extend(starts)
-        index_counts.extend(counts)
-
-    t.arguments = [
-        '--out', out_file,
-        '--verbose', stage_cfg['args']['verbose'],
-    ]
-
-    # Add list arguments
-    t.arguments.append('--in'); t.arguments.extend(in_files)
-    t.arguments.append('--start'); t.arguments.extend(index_starts)
-    t.arguments.append('--count'); t.arguments.extend(index_counts)
-
-    if global_cfg['print-help']:
-        t.arguments.extend(['-h'])
-
-    t.link_input_data = []
-    t.copy_output_data = []
-
-    if global_cfg['print-tasks-only']:
-        pprint(t.to_dict())
-
-    return t
-
-def task_sim_calc(i, month, stage_cfg, global_cfg, files_dims):
-    """
-    This function creates a similarity calculation task for the specified task number.
-
-    :param i: The task number.
-    :param month: The string for year and month, for example, 201001.
-    :param stage_cfg: The configuration dictionary for this stage.
-    :param global_cfg: The global configuration dictionary.
-    :param files_dims: The dimension information generated from the function get_files_dims.
-    :return: A Task object.
-    """
-
-    t = Task()
-    t.name = 'task-sims-calc-{:05d}'.format(i)
-
-    if global_cfg['print-progress']:
-        print "Creating similarity task {}".format(t.name)
-
-    t.pre_exec = stage_cfg['pre-exec']
-    t.executable = stage_cfg['executable']
-    t.cpu_threads = {
-        'processes': stage_cfg['cpu']['processes'],
-        'process-type': stage_cfg['cpu']['process-type'],
-        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
-        'thread-type': stage_cfg['cpu']['thread-type'],
-    }
-
-    # Extract the test month from the test forecast file path
-    test_month = extract_month(global_cfg['test-forecast-nc'])
-
-    # Calculate the indices for starts and counts
-    [test_starts, test_counts] = get_indices('forecasts', test_month, i, files_dims, global_cfg)
-    [search_starts, search_counts] = get_indices('forecasts', month, i, files_dims, global_cfg)
-    [obs_starts, obs_counts] = get_indices('observations', month, i, files_dims, global_cfg)
-
-    t.arguments = [
-        '--test-forecast-nc', global_cfg['test-forecast-nc'],
-        '--search-forecast-nc', '{}{}{}'.format(global_cfg['forecasts-folder'], month, '.nc'),
-        '--observation-nc', '{}{}{}'.format(global_cfg['observations-folder'], month, '.nc'),
-        '--similarity-nc', '{}{}-{:05d}{}'.format(global_cfg['sims-folder'], month, i, '.nc'),
-        '--verbose', stage_cfg['args']['verbose'],
-        '--observation-id', global_cfg['observation-id'],
-        '--sds-nc', '{}task-sd-calc-{:05d}{}'.format(global_cfg['sds-folder'], i, '.nc'),
-    ]
-
-    # Add list arguments
-    t.arguments.append('--test-start'); t.arguments.extend(test_starts)
-    t.arguments.append('--test-count'); t.arguments.extend(test_counts)
-    t.arguments.append('--search-start'); t.arguments.extend(search_starts)
-    t.arguments.append('--search-count'); t.arguments.extend(search_counts)
-    t.arguments.append('--obs-start'); t.arguments.extend(obs_starts)
-    t.arguments.append('--obs-count'); t.arguments.extend(obs_counts)
-
-    if global_cfg['print-help']:
-        t.arguments.extend(['-h'])
-
-    t.link_input_data = []
-    t.copy_output_data = []
-
-    if global_cfg['print-tasks-only']:
-        pprint(t.to_dict())
-
-    return t
-
-
-def create_analog_select_task(i, month, stage_cfg, global_cfg, files_dims):
-    """
-    This function creates a analog selection task for the specified task number.
-
-    :param i: The task number.
-    :param month: The string of year and month, for example, 201001.
-    :param stage_cfg: The configuration dictionary for this stage.
-    :param global_cfg: The global configuration dictionary.
-    :param files_dims: The dimension information generated from the function get_files_dims.
-    :return: A Task object.
-    """
-
-    t = Task()
-    t.name = 'task-analog-select-{:05d}'.format(i)
-
-    if global_cfg['print-progress']:
-        print "Creating analog selection task {}".format(t.name)
-
-    t.pre_exec = stage_cfg['pre-exec']
-    t.executable = stage_cfg['executable']
-    t.cpu_threads = {
-        'processes': stage_cfg['cpu']['processes'],
-        'process-type': stage_cfg['cpu']['process-type'],
-        'threads-per-process': stage_cfg['cpu']['threads-per-process'],
-        'thread-type': stage_cfg['cpu']['thread-type'],
-    }
-
-    # Calculate the indices for starts and counts
-    [index_starts, index_counts] = get_indices('observations', month, i, files_dims, global_cfg)
-
-    t.arguments = [
-        '--quick', stage_cfg['args']['quick'],
-        '--members', stage_cfg['args']['members'],
-        '--verbose', stage_cfg['args']['verbose'],
-        '--observation-id', global_cfg['observation-id'],
-
-        '--similarity-nc', '{}{}-{:05d}{}'.format(global_cfg['sims-folder'], month, i, '.nc'),
-        '--observation-nc', '{}{}{}'.format(global_cfg['observations-folder'], month, '.nc'),
-        '--analog-nc', '{}{}-{:05d}{}'.format(global_cfg['analogs-folder'], month, i, '.nc'),
-    ]
-
-    # Add list arguments
-    t.arguments.append('--obs-start'); t.arguments.extend(index_starts)
-    t.arguments.append('--obs-count'); t.arguments.extend(index_counts)
-
-    if global_cfg['print-help']:
-        t.arguments.extend(['-h'])
-
-    t.link_input_data = []
-    t.copy_output_data = []
-
-    if global_cfg['print-tasks-only']:
-        pprint(t.to_dict())
-
-    return t
-
+from radical.entk import Pipeline, Stage, AppManager
+from helpers.utils import get_files_dims, get_months_between, check_empty, expand_tilde
+from helpers.s1_task_sd_calc import task_sd_calc
+from helpers.s2_task_sim_calc import task_sim_calc
+from helpers.s3_task_analog_select import create_analog_select_task
+
+os.environ['RADICAL_PILOT_DBURL'] = 'mongodb://entk:entk123@dbh63.mlab.com:27637/anen'
 
 def create_pipelines(wcfg):
     """
@@ -413,18 +30,9 @@ def create_pipelines(wcfg):
 
     for i in range(wcfg['global']['task-count']):
         t = task_sd_calc(i, stage_cfg, wcfg['global'], files_dims)
-
-        if (wcfg['global']['run-serially']):
-            s = Stage()
-            s.name = 'stage-{}'.format(t.name)
-
         s.add_tasks(t)
 
-        if (wcfg['global']['run-serially']):
-            p.add_stages(s)
-
-    if (not wcfg['global']['run-serially']):
-        p.add_stages(s)
+    p.add_stages(s)
 
     # Create the stage for similarity calculator tasks
     s = Stage()
@@ -437,18 +45,9 @@ def create_pipelines(wcfg):
     for i in range(wcfg['global']['task-count']):
         for j in range(len(months)):
             t = task_sim_calc(i, months[j], stage_cfg, wcfg['global'], files_dims)
-
-            if (wcfg['global']['run-serially']):
-                s = Stage()
-                s.name = 'stage-{}'.format(t.name)
-
             s.add_tasks(t)
 
-            if (wcfg['global']['run-serially']):
-                p.add_stages(s)
-
-    if (not wcfg['global']['run-serially']):
-        p.add_stages(s)
+    p.add_stages(s)
 
     # Create the stage for analog selector tasks
     s = Stage()
@@ -458,18 +57,9 @@ def create_pipelines(wcfg):
     for i in range(wcfg['global']['task-count']):
         for j in range(len(months)):
             t = create_analog_select_task(i, months[j], stage_cfg, wcfg['global'], files_dims)
-
-            if (wcfg['global']['run-serially']):
-                s = Stage()
-                s.name = 'stage-{}'.format(t.name)
-
             s.add_tasks(t)
 
-            if (wcfg['global']['run-serially']):
-                p.add_stages(s)
-
-    if (not wcfg['global']['run-serially']):
-        p.add_stages(s)
+    p.add_stages(s)
 
     return p
 
@@ -518,11 +108,6 @@ if __name__ == '__main__':
         pipelines = [pipelines]
 
     amgr.workflow = pipelines
-
-    if wcfg['global']['run-serially']:
-        print("Each stage only has one task. Run tasks in serial.")
-    else:
-        print("Stages have multiple tasks. Run tasks in parallel.")
 
     if wcfg['global']['print-tasks-only']:
         print("Print tasks created only. Nothing has been run.")
