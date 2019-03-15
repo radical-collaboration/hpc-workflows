7bc2ca7ae0c47f45483acbd32fdde6d50886e731
Weiming-Hu <weiming@psu.edu>
2019-03-15 15:25:53 -0400

    change wc

---
+++ scripts/application_AnEn/year_2/multi-node/helpers/task_combine.py
@@ -0,0 +1,82 @@
+from utils import get_months_between, get_indices
+from radical.entk import Task
+from pprint import pprint
+import os
+import sys
+
+def task_combine(type, i, stage_cfg, global_cfg, along, files_dims):
+    """
+    This function combines the similarity files along time for a specific geographic region.
+
+    :param type: The file type to aggregate.
+    :param i: The task number.
+    :param stage_cfg: The configuration dictionary for this stage.
+    :param global_cfg: The global configuration dictionary.
+    :param along: Which dimension is appended to.
+    :return: A Task object.
+    """
+
+    t = Task()
+    t.name = 'task-{}_combine-{:05d}'.format(type, i)
+
+    data_folder = global_cfg['sims-folder']
+
+    if type is "Observations":
+        comb_file = '{}{:05d}{}'.format(data_folder, i, '.nc')
+    elif type is "Forecasts":
+        comb_file = '{}{:05d}{}'.format(data_folder, i, '.nc')
+    else:
+        print "Unsupported file type {}!".format(type)
+        sys.exit(1)
+
+    if os.path.isfile(comb_file):
+        print t.name + ": " + comb_file + " already exists. Skip generating this file!"
+        return False
+
+    if global_cfg['print-progress']:
+        print "Creating {} combination task {}".format(type, t.name)
+
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
+    t.cpu_reqs = {
+        'processes': stage_cfg['cpu']['processes'],
+        'process_type': stage_cfg['cpu']['process-type'],
+        'threads_per_process': stage_cfg['cpu']['threads-per-process'],
+        'thread_type': stage_cfg['cpu']['thread-type'],
+    }
+
+    # Get the number of forecast files to be used. This is the number of months specified.
+    months = get_months_between(global_cfg['search-month-start'], global_cfg['search-month-end'])
+
+    # Define the similarity files to combine
+    files_in = ['{}{}-{:05d}.nc'.format(data_folder, month, i) for month in months]
+
+    # Calculate the indices for starts and counts
+    index_starts = []; index_counts = []
+
+    for month in months:
+        [starts, counts] = get_indices('forecasts', month, i, files_dims, global_cfg)
+        index_starts.extend(starts)
+        index_counts.extend(counts)
+
+    t.arguments = [
+        '--type', type,
+        '--out', comb_file,
+        '--along', along, # Appending along the dimension num_entries
+        '--verbose', stage_cfg['args']['verbose'],
+    ]
+
+    t.arguments.append('--in'); t.arguments.extend(files_in)
+    t.arguments.append('--start'); t.arguments.extend(index_starts)
+    t.arguments.append('--count'); t.arguments.extend(index_counts)
+
+    if global_cfg['print-help']:
+        t.arguments.extend(['-h'])
+
+    t.link_input_data = []
+    t.copy_output_data = []
+
+    if global_cfg['print-tasks-only']:
+        pprint(t.to_dict())
+
+    return t
--- scripts/application_AnEn/year_2/multi-node/helpers/task_sim_calc.py
+++ scripts/application_AnEn/year_2/multi-node/helpers/task_sim_calc.py
@@ -3,6 +3,58 @@ from utils import extract_month, get_indices
 from pprint import pprint
 import os
 
+def task_sim_calc_v2(i, stage_cfg, global_cfg):
+    """
+    This function creates a similarity calculation task for the specified task number.
+
+    :param i: The task number.
+    :param stage_cfg: The configuration dictionary for this stage.
+    :param global_cfg: The global configuration dictionary.
+    :param files_dims: The dimension information generated from the function get_files_dims.
+    :return: A Task object.
+    """
+
+    t = Task()
+    t.name = 'task-sims-calc-{:05d}'.format(i)
+    sim_file = '{}{:05d}{}'.format(global_cfg['sims-folder'], i, '.nc')
+
+    if os.path.isfile(sim_file):
+        print t.name + ": " + sim_file + " already exists. Skip generating this file!"
+        return False
+
+    if global_cfg['print-progress']:
+        print "Creating similarity task {}".format(t.name)
+
+    t.pre_exec = stage_cfg['pre-exec']
+    t.executable = stage_cfg['executable']
+    t.cpu_reqs = {
+        'processes': stage_cfg['cpu']['processes'],
+        'process_type': stage_cfg['cpu']['process-type'],
+        'threads_per_process': stage_cfg['cpu']['threads-per-process'],
+        'thread_type': stage_cfg['cpu']['thread-type'],
+    }
+
+    t.arguments = [
+        '--similarity-nc', sim_file,
+        '--verbose', stage_cfg['args']['verbose'],
+        '--observation-id', global_cfg['observation-id'],
+        '--test-forecast-nc', global_cfg['test-forecast-nc'],
+        '--search-forecast-nc', '{}{:05d}{}'.format(global_cfg['forecasts-folder'], i, '.nc'),
+        '--observation-nc', '{}{:05d}{}'.format(global_cfg['observations-folder'], i, '.nc'),
+    ]
+
+    if global_cfg['print-help']:
+        t.arguments.extend(['-h'])
+
+    t.link_input_data = []
+    t.copy_output_data = []
+
+    if global_cfg['print-tasks-only']:
+        pprint(t.to_dict())
+
+    return t
+
+
 def task_sim_calc(i, month, stage_cfg, global_cfg, files_dims):
     """
     This function creates a similarity calculation task for the specified task number.
